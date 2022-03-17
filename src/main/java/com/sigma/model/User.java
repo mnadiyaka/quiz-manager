c443e35be2ecd32a9526567c3946b6fc8c2a763d
Nadiia Mishahina <mnadiyaka@gmail.com>
2022-03-17 10:29:57 +0200

    pom corrected, user model without methods

--- src/main/java/com/sigma/model/User.java
+++ src/main/java/com/sigma/model/User.java
@@ -1,8 +1,19 @@
 package com.sigma.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
     private int userid;
+
+    @Column(name = "user_name")
     private String username;
+
+    @Column(name = "password")
     private String password;
 //    private Role role; //enum
 }
