482363f99822ef17dd7148e59c42ed3d96b6c64a
Nadiia Mishahina <mnadiyaka@gmail.com>
2022-03-17 11:58:07 +0200

    imports corrected

--- src/main/java/com/sigma/model/Location.java
+++ src/main/java/com/sigma/model/Location.java
@@ -1,6 +1,11 @@
 package com.sigma.model;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import java.util.Objects;
 
 @Entity
--- src/main/java/com/sigma/model/Quiz.java
+++ src/main/java/com/sigma/model/Quiz.java
@@ -1,6 +1,13 @@
 package com.sigma.model;
 
-import javax.persistence.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
