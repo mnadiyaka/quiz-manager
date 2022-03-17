e47de0e3754dc1f2af2058a0c64378c9a6568bf2
Nadiia Mishahina <mnadiyaka@gmail.com>
2022-03-17 10:55:27 +0200

    location model is done

---
+++ src/main/java/com/sigma/model/Location.java
@@ -0,0 +1,89 @@
+package com.sigma.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "locations")
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "location_id")
+    private int locationId;
+
+    @Column(name = "location_name")
+    private String locationName;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "street")
+    private Role street;
+
+    @Column(name = "house_number")
+    private Role houseNumber;
+
+    @Column(name = "zip_code")
+    private Role zipCode;
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Role getStreet() {
+        return street;
+    }
+
+    public void setStreet(Role street) {
+        this.street = street;
+    }
+
+    public Role getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(Role houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public Role getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(Role zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Location location = (Location) o;
+        return locationId == location.locationId && Objects.equals(locationName, location.locationName) && Objects.equals(city, location.city) && street == location.street && houseNumber == location.houseNumber && zipCode == location.zipCode;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(locationId, locationName, city, street, houseNumber, zipCode);
+    }
+}
