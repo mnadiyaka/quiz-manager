c194ec0ca700872e3cf530cc6ef527ee1c5c48aa
Nadiia Mishahina <mnadiyaka@gmail.com>
2022-03-17 10:36:54 +0200

    user model is done

--- src/main/java/com/sigma/model/User.java
+++ src/main/java/com/sigma/model/User.java
@@ -15,5 +15,61 @@ public class User {
 
     @Column(name = "password")
     private String password;
-//    private Role role; //enum
+
+    @Column(name = "role")
+    private Role role;
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (userid != user.userid) return false;
+        if (!username.equals(user.username)) return false;
+        if (!password.equals(user.password)) return false;
+        return role == user.role;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = userid;
+        result = 31 * result + username.hashCode();
+        result = 31 * result + password.hashCode();
+        result = 31 * result + role.hashCode();
+        return result;
+    }
 }
