d7dbee71a794f0f7b1865dc2ca00f94af74b2ed7
Nadiia Mishahina <mnadiyaka@gmail.com>
2022-03-17 11:34:26 +0200

    quiz model is done

--- src/main/java/com/sigma/model/Quiz.java
+++ src/main/java/com/sigma/model/Quiz.java
@@ -2,6 +2,7 @@ package com.sigma.model;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Table(name = "quiz")
@@ -28,4 +29,64 @@ public class Quiz {
     @Column(name = "address_id")
     private Location address_id;
 
+    public int getQuizId() {
+        return quizId;
+    }
+
+    public void setQuizId(int quizId) {
+        this.quizId = quizId;
+    }
+
+    public String getQuizName() {
+        return quizName;
+    }
+
+    public void setQuizName(String quizName) {
+        this.quizName = quizName;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public Location getAddress_id() {
+        return address_id;
+    }
+
+    public void setAddress_id(Location address_id) {
+        this.address_id = address_id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Quiz quiz = (Quiz) o;
+        return quizId == quiz.quizId && quizName.equals(quiz.quizName) && category == quiz.category && shortDescription.equals(quiz.shortDescription) && dateTime.equals(quiz.dateTime) && address_id.equals(quiz.address_id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(quizId, quizName, category, shortDescription, dateTime, address_id);
+    }
 }
