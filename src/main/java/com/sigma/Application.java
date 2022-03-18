15ce4a8eddb69befc8eb1733884293942cced111
Nadiia Mishahina <mnadiyaka@gmail.com>
2022-03-18 19:48:19 +0200

    application properties added, main corrected, to pom added dependancy

--- src/main/java/com/sigma/Application.java
+++ src/main/java/com/sigma/Application.java
@@ -1,9 +1,10 @@
 package com.sigma;
 
+import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
