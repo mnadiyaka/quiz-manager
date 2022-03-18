7a1a38bfc802d0ea0a04de425b10c8f6986a5eb4
Nadiia Mishahina <mnadiyaka@gmail.com>
2022-03-18 15:00:36 +0200

    participant is done

--- src/main/java/com/sigma/model/Participant.java
+++ src/main/java/com/sigma/model/Participant.java
@@ -1,6 +1,15 @@
 package com.sigma.model;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.util.Objects;
 
 @Entity
 @Table(name = "participants")
@@ -8,7 +17,7 @@ public class Participant {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name="participant_id")
+    @Column(name = "participant_id")
     private int participantId;
 
     @Column(name = "firstname")
@@ -20,4 +29,49 @@ public class Participant {
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "team_id")
     private Team teamId;
+
+    public int getParticipantId() {
+        return participantId;
+    }
+
+    public void setParticipantId(int participantId) {
+        this.participantId = participantId;
+    }
+
+    public int getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(int firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Team getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Team teamId) {
+        this.teamId = teamId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Participant that = (Participant) o;
+        return participantId == that.participantId && firstname == that.firstname && lastname.equals(that.lastname) && teamId.equals(that.teamId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(participantId, firstname, lastname, teamId);
+    }
 }
