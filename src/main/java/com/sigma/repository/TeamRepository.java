5eab1582dc10ca7edcaec836c4b93ddee816dba1
Nadiia Mishahina <mnadiyaka@gmail.com>
2022-03-18 15:51:52 +0200

    team repo created

--- src/main/java/com/sigma/repository/TeamRepository.java
+++ src/main/java/com/sigma/repository/TeamRepository.java
@@ -1,8 +1,13 @@
 package com.sigma.repository;
 
+import com.sigma.model.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface TeamRepository extends JpaRepository {
+public interface TeamRepository extends JpaRepository <Team, Long> {
+
+    public List<Team> getTeamsByQuizId(Integer quizId);
 }
