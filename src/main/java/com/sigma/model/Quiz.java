b3c26b2a1c695480ee048caa048db7deeb2df643
Nadiia Mishahina <mnadiyaka@gmail.com>
2022-03-18 14:49:55 +0200

    quiz is done

--- src/main/java/com/sigma/model/Quiz.java
+++ src/main/java/com/sigma/model/Quiz.java
@@ -1,7 +1,14 @@
 package com.sigma.model;
 
-
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
@@ -9,6 +16,7 @@ import java.util.Objects;
 @Entity
 @Table(name = "quizzes")
 public class Quiz {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "quiz_id")
