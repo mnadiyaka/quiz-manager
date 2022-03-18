44600f1cd2b372bf5b1a75d8e92dbe607b097b78
Nadiia Mishahina <mnadiyaka@gmail.com>
2022-03-18 16:01:43 +0200

    location repo corrected

--- src/main/java/com/sigma/repository/LocationRepository.java
+++ src/main/java/com/sigma/repository/LocationRepository.java
@@ -9,5 +9,7 @@ import java.util.List;
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
 
+    public List<Location> getAllLocations();
+
     public List<Location> getLocationsByCity(String city);
 }
