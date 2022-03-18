d91defe9e56d0b2c664a8dfdb4b758803970dd9e
Nadiia Mishahina <mnadiyaka@gmail.com>
2022-03-18 15:54:26 +0200

    user repo created

---
+++ src/main/java/com/sigma/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.sigma.repository;
+
+import com.sigma.model.Role;
+import com.sigma.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    public List<User> getUsersByRole(Role role);
+}
