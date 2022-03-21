f2c1257e71a43b6c9bce79d9658e2bd899906b9e
Nadiia Mishahina <mnadiyaka@gmail.com>
2022-03-21 13:13:35 +0200

    teamRepo corrected

--- src/main/java/com/sigma/repository/TeamRepository.java
+++ src/main/java/com/sigma/repository/TeamRepository.java
@@ -9,5 +9,5 @@ import java.util.List;
 @Repository
 public interface TeamRepository extends JpaRepository <Team, Long> {
 
-    //public List<Team> findTeamsByQuizId(Integer quizId);
+    public List<Team> findTeamsByQuizzesContaining(Integer quizId);
 }
