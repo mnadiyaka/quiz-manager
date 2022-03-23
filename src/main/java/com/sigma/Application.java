45089a7547e73c0c3dafad5f1403df9e7d537a24
Nadiia Mishahina <mnadiyaka@gmail.com>
2022-03-23 17:00:55 +0200

    pom & aplication

--- src/main/java/com/sigma/Application.java
+++ src/main/java/com/sigma/Application.java
@@ -1,10 +1,9 @@
 package com.sigma;
 
-import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
