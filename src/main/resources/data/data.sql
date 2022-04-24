1ca7f103a92a4b5129dabf651ad1b53fcd932e36
Nadiia Mishahina <mnadiyaka@gmail.com>
2022-04-24 15:57:53 +0300

    insert data user added

---
+++ src/main/resources/data/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO users(user_id, user_name, password, role) VALUES (10001, 'Nadiia', '$2b$10$ZDpLQEq4i.ELlzx6OFcoSuWpTPIq0KPXYZwfm7qtIF9DQgyvW1dOW', 0;
+INSERT INTO users(user_id, user_name, password, role) VALUES (10002, 'captain1', '$2b$10$FQKU13Eom4SxaiNGmrtereuIvOkobfFCyIUwSGL54zEMz9sUbRgX6', 1);
+INSERT INTO users(user_id, user_name, password, role) VALUES (10003, 'captain2', '$2b$10$vtpxxRusAXB2RFdc4cGd9uk2sExZQrkkEB/rtx7BBKIrBvOsbnE0e', 1);
+INSERT INTO users(user_id, user_name, password, role) VALUES (10004, 'captain0', '$2b$10$1.n.dyAOMnHLhdnZ.v9she10AViYaAUa8TACn7W6UBleom8IyAcz.', 1);
+INSERT INTO users(user_id, user_name, password, role) VALUES (10005, 'captain3', '$2b$10$3vs1UT.67BjxjYMqDBB4z.iS07gMxUEngDk4ekUaV2tSlHgBHHPUS', 1);
+INSERT INTO users(user_id, user_name, password, role) VALUES (10006, 'captainTest0', '$2b$10$NGnPQDlXvH3X6nal1phi0.6prlMixeiUZC6CajmCWCnKm2ApRtbMa', 1);
+INSERT INTO users(user_id, user_name, password, role) VALUES (10007, 'captainTest00', '$2b$10$kDt4ZDJ2QXmSCm2wlEF2Q.AUEpEZf82o.AMi7BZu33/LFIhDUT8nW', 1);
+
