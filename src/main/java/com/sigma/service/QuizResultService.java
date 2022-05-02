7102af8da8aebb8b9d7b016475c2d637e905ad54
Nadiia Mishahina <mnadiyaka@gmail.com>
2022-05-02 23:04:39 +0300

    service

---
+++ src/main/java/com/sigma/service/QuizResultService.java
@@ -0,0 +1,22 @@
+package com.sigma.service;
+
+import com.sigma.model.dto.QuizResultsDto;
+import com.sigma.model.entity.QuizResults;
+
+import java.util.Map;
+import java.util.Set;
+
+public interface QuizResultService {
+
+    QuizResults createRes(QuizResults newQuizResults);
+
+    QuizResults updateRes(QuizResultsDto newQuizResults);
+
+    void deleteRes(Long id);
+
+    Set<QuizResultsDto> getAllRes();
+
+    QuizResults findResById(Long id);
+
+    Set<QuizResultsDto> filterData(Map<String, String> data);
+}
