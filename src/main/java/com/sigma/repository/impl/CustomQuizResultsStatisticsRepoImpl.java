272d6866a08460c7a1a7642653d76bc780ca6c3d
Nadiia Mishahina <mnadiyaka@gmail.com>
2022-05-16 12:48:00 +0300

    filtering quizzes corrected

--- src/main/java/com/sigma/repository/impl/CustomQuizResultsStatisticsRepoImpl.java
+++ src/main/java/com/sigma/repository/impl/CustomQuizResultsStatisticsRepoImpl.java
@@ -8,8 +8,10 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Repository
 public class CustomQuizResultsStatisticsRepoImpl implements CustomQuizResultsStatisticsRepo {
@@ -20,20 +22,21 @@ public class CustomQuizResultsStatisticsRepoImpl implements CustomQuizResultsSta
     @Override
     public List<QuizResults> findResultsWithCustomQuery(QuizResultsSearchDto data) {
 
-        final StringBuilder query = new StringBuilder("SELECT r.id, r.quiz_id, r.team_id, r.score " +
-                "FROM quiz_results r JOIN quizzes q on r.quiz_id = q.quiz_id ");
-        Optional.ofNullable(data).ifPresent(data1-> {
-            query.append("WHERE");
-            Optional.ofNullable(data1.getLocationId()).ifPresent((el) -> query.append(" AND address_id = " + el));
-            Optional.ofNullable(data1.getCategory()).ifPresent((el) -> query.append(" AND category = " + el.ordinal()));
-            Optional.ofNullable(data1.getDate()).ifPresent(date -> {
-                query.append(" AND datetime >= '" + date + "'");
-                Optional.ofNullable(data1.getPeriod()).ifPresent((el) -> query.append(" AND datetime <= '" + date.plusDays(el.getDays()) + "'"));
+        String query = "SELECT r.id, r.quiz_id, r.team_id, r.score " +
+                "FROM quiz_results r JOIN quizzes q on r.quiz_id = q.quiz_id ";
+        List<String> params = new ArrayList<>();
+        if (data.checkExistence()) {
+            query += "WHERE ";
+            Optional.ofNullable(data.getLocationId()).ifPresent((el) -> params.add("address_id = " + el));
+            Optional.ofNullable(data.getCategory()).ifPresent((el) -> params.add("category = " + el.ordinal()));
+            Optional.ofNullable(data.getDate()).ifPresent(date -> {
+                params.add("datetime >= '" + date + "'");
+                Optional.ofNullable(data.getPeriod()).ifPresent((el) -> params.add("datetime <= '" + date.plusDays(el.getDays()) + "'"));
             });
-        });
-        query.append(";");
-
-        final Query q = entityManager.createNativeQuery(query.toString().replace("WHERE AND", "WHERE").replace("WHERE;", ";"), QuizResults.class);
+        }
+        query += params.stream().collect(Collectors.joining(" AND "));
+        query += ";";
+        final Query q = entityManager.createNativeQuery(query.toString(), QuizResults.class);
 
         List<QuizResults> userList = q.getResultList();
 
