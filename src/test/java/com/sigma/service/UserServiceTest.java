922e3d6e765f9ec1e9e25ddb386bc4f6ca899ef8
Nadiia Mishahina <mnadiyaka@gmail.com>
2022-05-20 18:26:36 +0300

    some tests for user

--- src/test/java/com/sigma/service/UserServiceTest.java
+++ src/test/java/com/sigma/service/UserServiceTest.java
@@ -1,82 +1,137 @@
 package com.sigma.service;
 
+import com.sigma.configuration.auth.TokenAuth;
+import com.sigma.model.dto.SignInUserDto;
 import com.sigma.model.dto.SignUpUserDto;
+import com.sigma.model.dto.SignUpUserResponseDto;
+import com.sigma.model.dto.TeamDto;
+import com.sigma.model.dto.UserDto;
 import com.sigma.model.entity.Role;
+import com.sigma.model.entity.Team;
 import com.sigma.model.entity.User;
+import com.sigma.repository.ParticipantRepository;
+import com.sigma.repository.TeamRepository;
 import com.sigma.repository.UserRepository;
+import com.sigma.service.impl.ParticipantServiceImpl;
+import com.sigma.service.impl.TeamServiceImpl;
+import com.sigma.service.impl.UserServiceImpl;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.transaction.Transactional;
+import javax.persistence.EntityNotFoundException;
 
-@SpringBootTest
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 public class UserServiceTest {
 
-    /*@Autowired
     private UserRepository userRepository;
 
-    @Autowired
     private UserService userService;
 
-    @Test
-    @Transactional
-    public void findUserById() {
-        User user = userService.findUserById(1L);
+    private PasswordEncoder passwordEncoder;
 
-        Assertions.assertEquals(user.getUsername(), "adminn");
+    @BeforeEach
+    void setUp() {
+        userRepository = mock(UserRepository.class);
+        passwordEncoder = new BCryptPasswordEncoder();
+        userService = new UserServiceImpl(userRepository, passwordEncoder);
     }
 
     @Test
-    @Transactional
-    public void createUserTest() {
-        int expected = userService.getAllUsers().size();
+    public void findUserById_WithExistingId_ThenReturnUser() {
+        final User expectedResult = new User();
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(expectedResult));
+        final User result = userService.findUserById(anyLong());
 
-        SignUpUserDto user = new SignUpUserDto();
-        user.setUsername("user1");
-        user.setPassword("12345");
-        user.setRole(Role.ROLE_CAPTAIN);
-
-        userService.createUser(user);
-        int actual = userService.getAllUsers().size();
-
-        Assertions.assertEquals(expected+1, actual);
+        Assertions.assertEquals(expectedResult, result);
+        verify(userRepository, times(1)).findById(anyLong());
     }
 
     @Test
-    @Transactional
-    public void updateUserTest() {
-        User user = userService.findUserById(1L);
-        user.setRole(Role.CAPTAIN);
+    public void findUserById_WithNonExistingId_ThenThrowException() {
+        when(userRepository.findById(anyLong())).thenThrow(EntityNotFoundException.class);
 
-        userService.updateUser(user, user.getId());
-        Assertions.assertEquals(user, userService.findUserById(1L));
+        Assertions.assertThrows(EntityNotFoundException.class, () -> userService.findUserById(anyLong()));
     }
 
     @Test
-    @Transactional
-    public void deleteUser() {
-        int expected = userService.getAllUsers().size();
+    public void createUserTest_WithUser_ThenReturnNewUser() {//TODO: SMTH With Encode
+        SignUpUserResponseDto expected = new SignUpUserResponseDto("success", "user created");
+
+        SignUpUserDto signUpUserDto = new SignUpUserDto();
+        signUpUserDto.setUsername("new");
+        signUpUserDto.setPassword("new");
+        signUpUserDto.setRole(Role.CAPTAIN);
+
+        User user = new User(signUpUserDto.getUsername(), passwordEncoder.encode(signUpUserDto.getPassword()), Role.CAPTAIN);
+        when(this.userRepository.save(user)).thenReturn(user);
+
+        SignUpUserResponseDto actual = userService.createUser(signUpUserDto);
+        Assertions.assertEquals(expected, actual);
+        verify(this.userRepository, times(1)).save(user);
+    }
+
+    @Test
+    public void updateUserTest_WithUpdatedUserAndId_ThenReturnUpdatedUser() {
+        User team = new User();
+        team.setId(1L);
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(team));
+
+        User oldUser = userService.findUserById(1L);
+        oldUser.setUsername("new");
+
+        when(userRepository.save(oldUser)).thenReturn(oldUser);
+
+        User actual = userService.updateUser(oldUser, 1L);
+        Assertions.assertEquals(oldUser, actual);
+        verify(userRepository, times(1)).save(oldUser);
+    }
+
+    @Test
+    public void deleteUser_WithCorrectCaptain() {
+        User expected = new User();
+        expected.setId(1L);
+        when(userRepository.findById(anyLong())).thenReturn(Optional.of(expected));
+
         userService.deleteUser(1L);
-        int actual = userService.getAllUsers().size();
-        Assertions.assertEquals(expected - 1, actual);
+        verify(userRepository).delete(expected);
     }
 
     @Test
-    @Transactional
-    public void getAllUsers() {
-        int expected = userRepository.findAll().size();
+    public void deleteUser_WithIncorrectCaptain_ThenThrowException() {
+        User expected = new User();
+        expected.setId(1L);
+        when(userRepository.findById(anyLong())).thenThrow(EntityNotFoundException.class);
 
-        SignUpUserDto user = new SignUpUserDto();
-        user.setUsername("user1");
-        user.setPassword("12345");
-        user.setRole(Role.CAPTAIN);
+        Assertions.assertThrows(EntityNotFoundException.class, () -> userService.deleteUser(anyLong()));
+        verify(userRepository, times(0)).delete(expected);
+    }
 
-        userService.createUser(user);
+    @Test
+    public void getAllUsers_WithUserId_ThenReturnList() {
+        List<User> teams = new ArrayList<>();
+        teams.add(new User());
+        teams.add(new User());
+        teams.add(new User());
+        when(userRepository.findAll()).thenReturn(teams);
 
-        int actual = userService.getAllUsers().size();
-
-        Assertions.assertEquals(expected + 1, actual);
-    }*/
+        List<UserDto> actual = userService.getAllUsers();
+        Assertions.assertEquals(teams.stream().map(UserDto::fromUser).collect(Collectors.toList()), actual);
+        verify(userRepository, times(1)).findAll();
+    }
 }
