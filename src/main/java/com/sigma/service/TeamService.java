e75235793adf3411207007d071dcb0f4a89d391a
Nadiia Mishahina <mnadiyaka@gmail.com>
2022-06-01 11:10:45 +0300

    teamervice part docs added

--- src/main/java/com/sigma/service/TeamService.java
+++ src/main/java/com/sigma/service/TeamService.java
@@ -1,28 +1,44 @@
 package com.sigma.service;
 
+import com.sigma.exception.QuizException;
 import com.sigma.model.dto.ParticipantDto;
 import com.sigma.model.dto.TeamDto;
 import com.sigma.model.entity.Quiz;
 import com.sigma.model.entity.Team;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 public interface TeamService {
-    public List<TeamDto> getAllTeams();
+    List<TeamDto> getAllTeams();
 
-    public Team findTeamById(Long teamId);
+    Team findTeamById(Long teamId);
 
-    public Team createTeam(TeamDto teamDto);
+    Team createTeam(TeamDto teamDto);
 
-    public Team updateTeam(TeamDto updatedTeam, Long teamId);
+    Team updateTeam(TeamDto updatedTeam, Long teamId);
 
-    public void deleteTeam(Long teamId);
+    void deleteTeam(Long teamId);
 
-    public Team confirmTeam(Team team, boolean confirmation); //TODO: DTO????
+    Team confirmTeam(Team team, boolean confirmation);
 
-    public void updatePl(final ParticipantDto newParticipant, final Long participantId, final Long teamId);
+    void updatePl(final ParticipantDto newParticipant, final Long participantId, final Long teamId);
 
-    public void addPl(final ParticipantDto newParticipant, final Long teamId);
+    /**
+     * Adds new player to exicting team
+     * @param newParticipant New Player
+     * @param teamId
+     */
+    void addPl(final ParticipantDto newParticipant, final Long teamId);
 
-    public Team applyForQuiz(final Quiz quiz, final Long teamId);
+    /**
+     * Works with quizService to assign existing Team for a Quiz
+     * @param quiz Existing Quiz
+     * @param teamId Existing Team id
+     * @throws EntityNotFoundException If entered Quiz id does not exist
+     * @throws QuizException If Quiz is not ANNOUNCED yet
+     * @throws QuizException If Max Number of teams is reached
+     * @throws QuizException If Number of Participants doesn't suit criteria
+     */
+    Team applyForQuiz(final Quiz quiz, final Long teamId);
 }
