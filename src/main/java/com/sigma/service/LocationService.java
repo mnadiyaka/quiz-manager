a58f378fa0c1a2118d4a028d93d96c5f63362151
Nadiia Mishahina <mnadiyaka@gmail.com>
2022-06-01 10:21:14 +0300

    participantService docs added

--- src/main/java/com/sigma/service/LocationService.java
+++ src/main/java/com/sigma/service/LocationService.java
@@ -12,22 +12,22 @@ public interface LocationService {
      * Finds all possible Locations for future or current quiz
      * @return All Locations
      */
-    public List<LocationDto> getAllLocations();
+    List<LocationDto> getAllLocations();
 
     /**
      * Finds location by entered existing id
-     * @param locationId possible existing location
-     * @return found Location
-     * @throws EntityNotFoundException if entered id is not correct
+     * @param locationId Possible existing location
+     * @return Found Location
+     * @throws EntityNotFoundException If entered id is not correct
      */
-    public Location findLocationById(Long locationId);
+    Location findLocationById(Long locationId);
 
     /**
      * Creates new location from entered Data
      * @param location Entered data
      * @return Created new Location
      */
-    public Location createLocation(LocationDto location);
+    Location createLocation(LocationDto location);
 
     /**
      * Tries to update existing Location, if it doesn't exist - creates new one
@@ -35,12 +35,12 @@ public interface LocationService {
      * @param locationId Location's id
      * @return Updated Location
      */
-    public Location updateLocation(LocationDto updatedLocation, Long locationId);
+    Location updateLocation(LocationDto updatedLocation, Long locationId);
 
     /**
      * Deletes Location by id
      * @param locationId Location's id
-     * @throws EntityNotFoundException if Location is not found
+     * @throws EntityNotFoundException If Location is not found
      */
-    public void deleteLocation(Long locationId);
+    void deleteLocation(Long locationId);
 }
--- src/main/java/com/sigma/service/ParticipantService.java
+++ src/main/java/com/sigma/service/ParticipantService.java
@@ -3,17 +3,50 @@ package com.sigma.service;
 import com.sigma.model.dto.ParticipantDto;
 import com.sigma.model.entity.Participant;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 public interface ParticipantService {
 
-    public List<ParticipantDto> getAllParticipants(Long teamId);
+    /**
+     * Finds all Participants for selected Team
+     * @param teamId Team's id to select from
+     * @return List of Participants
+     */
+    List<ParticipantDto> getAllParticipants(Long teamId);
 
-    public Participant findParticipantById(final Long teamId, Long participantId);
+    /**
+     * Finds Participant by entered existing id
+     * @param teamId Possible existing Team
+     * @param participantId Possible existing Participant
+     * @return Found Participant
+     * @throws EntityNotFoundException If Participant's id doesn't exist for chosen team
+     */
+    Participant findParticipantById(final Long teamId, Long participantId);
 
-    public Participant createParticipant(final ParticipantDto participantDto, final Long teamId);
+    /**
+     * Creates new Participant from entered Data
+     * @param participantDto Entered data
+     * @param teamId Team's id, whose Participant will be created
+     * @return New Participant
+     */
+    Participant createParticipant(final ParticipantDto participantDto, final Long teamId);
 
-    public Participant updateParticipant(ParticipantDto newParticipant, Long participantId, Long teamId);
+    /**
+     * Tries to update existing Participant, if it exists
+     * @param newParticipant Updated data
+     * @param participantId Participant's id
+     * @param teamId Team's id
+     * @return Updated Participant
+     * @throws EntityNotFoundException If Participant's id doesn't exist for chosen team
+     */
+    Participant updateParticipant(ParticipantDto newParticipant, Long participantId, Long teamId);
 
-    public void deleteParticipant(Long teamId, Long participantId);
+    /**
+     * Deletes by Participant's id and Team's id
+     * @param teamId Team's id
+     * @param participantId Participant's id
+     * @throws EntityNotFoundException If Participant's id doesn't exist for chosen team
+     */
+    void deleteParticipant(Long teamId, Long participantId);
 }
